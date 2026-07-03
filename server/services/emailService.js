587721b8564d916270efc45104e7e45c106a228a
Patel Arya <286640538+apatelx19@users.noreply.github.com>
2026-07-03 13:13:30 +0530

    Use URLSearchParams to send urlencoded token request for Gmail API

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -33,11 +33,16 @@ class EmailService {
 
   async getGmailAccessToken() {
     try {
-      const response = await axios.post('https://oauth2.googleapis.com/token', {
-        client_id: process.env.GMAIL_CLIENT_ID,
-        client_secret: process.env.GMAIL_CLIENT_SECRET,
-        refresh_token: process.env.GMAIL_REFRESH_TOKEN,
-        grant_type: 'refresh_token'
+      const params = new URLSearchParams();
+      params.append('client_id', process.env.GMAIL_CLIENT_ID);
+      params.append('client_secret', process.env.GMAIL_CLIENT_SECRET);
+      params.append('refresh_token', process.env.GMAIL_REFRESH_TOKEN);
+      params.append('grant_type', 'refresh_token');
+
+      const response = await axios.post('https://oauth2.googleapis.com/token', params, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       });
       return response.data.access_token;
     } catch (error) {
